d1389ac51bf8bc3e08d20801df67256c78b3d5d2
Jan Smółka <jsmolka@student.agh.edu.pl>
2024-11-23 18:50:26 +0100

    Removed unused functionalities from `core`

--- child_lab_framework/core/stream.py
+++ child_lab_framework/core/stream.py
@@ -1,14 +1,1 @@
-from functools import wraps
-
-
 class InvalidArgumentException(Exception): ...
-
-
-def autostart(method):
-    @wraps(method)
-    def inner(self):
-        # TODO: implement properly for async generators
-        gen = method(self)
-        return gen
-
-    return inner
