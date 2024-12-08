93603839c717a7b38df845b21f8268d292dbb8e2
Jan Smółka <jp.smolka@gmail.com>
2024-12-08 13:17:16 +0100

    Renamed `social_distance` to `social_proximity` (#77)

--- child_lab_framework/task/social_distance/__init__.py
+++
@@ -1,3 +0,0 @@
-from .social_distance import Estimator, FileLogger, Result
-
-__all__ = ['Estimator', 'FileLogger', 'Result']
---
+++ child_lab_framework/task/social_proximity/__init__.py
@@ -0,0 +1,3 @@
+from .social_proximity import Estimator, FileLogger, Result
+
+__all__ = ['Estimator', 'FileLogger', 'Result']
