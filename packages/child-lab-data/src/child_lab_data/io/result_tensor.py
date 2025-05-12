22e07d3ce5b21c7ed1aca3415ab615e7be0dbd26
Jan Smółka <jp.smolka@gmail.com>
2025-05-12 23:14:45 +0200

    Implement readers and writers for point clouds and `vpc` results (#175)

---
+++ packages/child-lab-data/src/child_lab_data/io/result_tensor.py
@@ -0,0 +1,67 @@
+import logging
+from itertools import count
+from pathlib import Path
+
+import torch
+import vpc
+from beartype import beartype
+
+from child_lab_data.serde.result_tensor import deserialize, serialize
+
+type Readable = vpc.pose.Result | vpc.pose.Result3d | vpc.gaze.Result3d
+type Writeable = vpc.pose.Result | vpc.pose.Result3d | vpc.gaze.Result3d
+
+
+class Reader[T: Readable]:
+    ty: type[T]
+    directory: Path
+    counter: 'count[int]'
+
+    def __init__(self, ty: type[T], directory: Path) -> None:
+        self.ty = ty
+        self.directory = directory
+        self.counter = count()
+
+    def skip(self, count: int) -> None:
+        for _ in range(count):
+            next(self.counter)
+
+    @beartype
+    def read(self, id: int | None = None) -> T | None:
+        if id is None:
+            id = next(self.counter)
+
+        path = self.directory / f'{id}.pt'
+        if not path.is_file():
+            logging.warning(f'Serialized result not found at {path}')
+            return None
+
+        tensor: torch.Tensor = torch.load(path, weights_only=True)
+        return deserialize(self.ty, tensor)
+
+
+class Writer[T: Writeable]:
+    ty: type[T]
+    directory: Path
+    counter: 'count[int]'
+
+    def __init__(self, ty: type[T], directory: Path) -> None:
+        self.ty = ty
+        self.directory = directory
+        self.counter = count()
+
+    def skip(self, count: int) -> None:
+        for _ in range(count):
+            next(self.counter)
+
+    @beartype
+    def write(self, item: T, id: int | None = None) -> None:
+        if id is None:
+            id = next(self.counter)
+
+        path = self.directory / f'{id}.ply'
+        if path.is_file():
+            logging.warning(f'Overwriting the existing result at {path}')
+
+        tensor = serialize(item)
+        torch.save(tensor, path)
---
+++ packages/child-lab-data/src/child_lab_data/serde/result_tensor.py
@@ -0,0 +1,64 @@
+import torch
+from beartype import beartype
+from jaxtyping import Float
+from plum import dispatch, overload
+from vpc import gaze, pose
+
+
+@overload
+def serialize(item: pose.Result3d) -> Float[torch.Tensor, 'n_detections 74']:  # noqa: F811
+    flat_boxes = item.boxes  # n_detections x 6
+    flat_keypoints = item.keypoints.flatten(1, -1)  # n_detections x 17 * 4
+    return torch.cat((flat_boxes, flat_keypoints), dim=1)
+
+
+@overload
+def deserialize(  # noqa: F811
+    ty: type[pose.Result3d],
+    item: Float[torch.Tensor, 'n_detections 74'],
+) -> pose.Result3d:
+    boxes = item[:, :6, :]
+    keypoints = item[:, 6:, :].unflatten(-1, (17, 4))
+    return pose.Result3d(boxes, keypoints, item.device)
+
+
+@overload
+def serialize(item: pose.Result) -> Float[torch.Tensor, 'n_detections 56']:  # noqa: F811
+    flat_boxes = item.boxes  # n_detections x 5
+    flat_keypoints = item.keypoints.flatten(1, -1)  # n_detections x 17 * 3
+    return torch.cat((flat_boxes, flat_keypoints), dim=1)
+
+
+@overload
+def deserialize(  # noqa: F811
+    ty: type[pose.Result],
+    item: Float[torch.Tensor, 'n_detections 56'],
+) -> pose.Result:
+    boxes = item[:, :6, :]
+    keypoints = item[:, 6:, :].unflatten(-1, (17, 3))
+    return pose.Result(boxes, keypoints, item.device)
+
+
+@overload
+def serialize(item: gaze.Result3d) -> Float[torch.Tensor, 'n_detections 4 3']:  # noqa: F811
+    return torch.cat((item.eyes, item.directions), dim=1)
+
+
+@overload
+def deserialize(  # noqa: F811
+    ty: type[gaze.Result3d],
+    item: Float[torch.Tensor, 'n_detections 4 3'],
+) -> gaze.Result3d:
+    eyes = item[:, :2, :]
+    directions = item[:, 2:, :]
+    return gaze.Result3d(eyes, directions, item.device)
+
+
+@beartype
+@dispatch
+def serialize(item: object) -> torch.Tensor: ...  # noqa: F811
+
+
+@beartype
+@dispatch
+def deserialize(ty: type, item: object) -> object: ...  # noqa: F811
